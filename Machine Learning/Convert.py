4549cbf7ceed453da659be68d896e1aca314b2e8
akbar_wibowo <akbarwibowo0123@gmail.com>
2023-12-22 10:35:26 +0700

    re-convert model.tflite

--- Machine Learning/Convert.py
+++ Machine Learning/Convert.py
@@ -12,12 +12,19 @@ truncating = 'post'
 
 # model = keras.models.load_model('1')
 
-converter = tf.lite.TFLiteConverter.from_saved_model('1')
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
-converter._experimental_lower_tensor_list_ops = False  # Disable experimental lowering
+# converter = tf.lite.TFLiteConverter.from_saved_model('1')
+# converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
+# converter._experimental_lower_tensor_list_ops = False  # Disable experimental lowering
+# tflite_model = converter.convert()
+
+converter = tf.lite.TFLiteConverter.from_saved_model('./1')
+converter.target_spec.supported_ops = [
+    tf.lite.OpsSet.TFLITE_BUILTINS,  # enable TensorFlow Lite ops.
+    tf.lite.OpsSet.SELECT_TF_OPS  # enable TensorFlow ops.
+]
+converter.experimental_enable_resource_variables = True
 tflite_model = converter.convert()
+
 
 with open("model.tflite", "wb") as f:
     f.write(tflite_model)
-
-
