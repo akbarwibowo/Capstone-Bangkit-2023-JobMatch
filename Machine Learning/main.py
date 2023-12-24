8747227a056c3e21e18f1940e4fbe1ab767252b4
akbar_wibowo <akbarwibowo0123@gmail.com>
2023-12-24 18:22:15 +0700

    fix HTTP request for prediction. worked server

--- Machine Learning/main.py
+++ Machine Learning/main.py
@@ -1,38 +1,88 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-import io
-import tensorflow as tf
+import json
+from keras.preprocessing.text import tokenizer_from_json
 from tensorflow import keras
 from flask import Flask, request, jsonify
 import pandas as pd
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
 
-model = keras.models.load_model("model.h5")
+maxlen = 500
+padding = 'post'
+truncating = 'post'
 
-def get_job_recommendation(user_data):
-    # Process user data (adjust as needed based on your model's input requirements)
-    # For example, convert user input to the format expected by your model
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-    # Perform inference using the loaded model
-    try:
-        predictions = model.predict(user_data)
-        # You might need additional post-processing based on your model's output
-        return predictions.tolist()  # Convert predictions to a JSON-serializable format
-    except Exception as e:
-        return {'error': str(e)}
+job_label = pd.read_csv("./Machine Learning/Dataset/data_capstone.csv", delimiter=";")
+job_label = job_label['job_title']
+job_label = job_label[:425]
+
+model = keras.models.load_model("./Machine Learning/model.h5")
+
+
+def load_tokenizer(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        tokenizer = json.load(f)
+        tokenizer = tokenizer_from_json(tokenizer)
+    return tokenizer
+
+
+degree_token = load_tokenizer("./Machine Learning/degree_token.json")
+job_token = load_tokenizer("./Machine Learning/job_token.json")
+key_token = load_tokenizer("./Machine Learning/key_token.json")
+
+
+def process_input(degree, job_exp, key_skill):
+    input_degree_seq = degree_token.texts_to_sequences(degree)
+    input_degree_pad = pad_sequences(input_degree_seq, maxlen=maxlen, padding=padding, truncating=truncating)
+    input_job_seq = job_token.texts_to_sequences(job_exp)
+    input_job_pad = pad_sequences(input_job_seq, maxlen=maxlen, padding=padding, truncating=truncating)
+    input_key_seq = key_token.texts_to_sequences(key_skill)
+    input_key_pad = pad_sequences(input_key_seq, maxlen=maxlen, padding=padding, truncating=truncating)
+    return input_degree_pad, input_job_pad, input_key_pad
+
+
+def predict(input_degree, input_job, input_key):
+    predictions = model.predict(
+        {
+            'degree': input_degree,
+            'job': input_job,
+            'key': input_key,
+        }
+    )
+    predicted_label = np.argmax(predictions, axis=1)
+    label = [job_label[i] for i in predicted_label]
+    return label
+
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
-@app.route("/", methods=["POST"])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    file = request.files.get('file')
-    if file is None or file.filename == "":
-        return jsonify({"error": "No file provided"})
+    if request.method == "POST":
+        file = request.files.get('file')
+        if file is None or file.filename == "":
+            return jsonify({"error": "No file provided"})
 
-    # Process the file as needed
+        try:
+            form_data = json.load(file)
 
-    return jsonify({"success": True})
+            degree = str(form_data['degree'])
+            job = str(form_data['job'])
+            key = str(form_data['key'])
+
+            if None in [degree, job, key]:
+                return jsonify({"error": "Missing required form data"})
+
+            degree_pad, job_pad, key_pad = process_input([degree], [job], [key])
+            label = predict(degree_pad, job_pad, key_pad)
+
+            return jsonify({"label": label})
+        except Exception as e:
+            return jsonify({"error": str(e)})
+    return jsonify({'get': 'clear'})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Machine Learning/test.py
+++ Machine Learning/test.py
@@ -1,11 +1,8 @@
 import requests
 
-# Read the content of the text file
-with open('input.txt', 'r') as file:
-    file_content = file.read()
-
 # Send the file content in the request body using the 'data' parameter
-resp = requests.post("http://127.0.0.1:5000", files={'file': open('input.txt', 'r')})
+resp = requests.post("http://127.0.0.1:5000/", files={'file': open('test.json', 'r', encoding='utf-8')})
+# resp = requests.get("http://127.0.0.1:5000/")
 
 # Print the response
 print(resp.json())
